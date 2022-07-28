7b02c65c805bf237986feee1181d6c060f5c9d9f
Meghana <vnslmeghana@gmail.com>
2022-07-28 12:20:46 +0530

    second commit with usecase

--- src/App.js
+++ src/App.js
@@ -5,9 +5,15 @@ import Food from './components/foodcomponent';
 function App() {
   return (
     <div className='App'>
-      <Food name='Pizza' price='450'img='https://source.unsplash.com/1600x800/?pizza'/>
+      <Food name='Pizza' price='450'img='https://source.unsplash.com/1600x800/?pasta'/>
       <Food name='Idly'price='100' img='https://source.unsplash.com/1600x800/?Idly'/>
       <Food name='Dosa' price='120'img='https://source.unsplash.com/1600x800/?Dosa'/>
+      <Food name='chocolate' price='120'img='https://source.unsplash.com/1600x800/?chocolate'/>
+      <Food name='sweet' price='120'img='https://source.unsplash.com/1600x800/?sweet'/>
+      <Food name='icecream' price='120'img='https://source.unsplash.com/1600x800/?icecream'/>
+      <Food name='burger' price='120'img='https://source.unsplash.com/1600x800/?burger'/>
+      <Food name='milkshake' price='120'img='https://source.unsplash.com/1600x800/?masala'/>
+      <Food name='biryani' price='120'img='https://source.unsplash.com/1600x800/?biryani'/>
     </div>
   );
 }
--- src/components/foodcomponent.js
+++ src/components/foodcomponent.js
@@ -1,20 +1,25 @@
 import './foodcomponent.css'
-function food(props){
+import React,{useState} from 'react';
+function Food(props){
+    const[btntext,setbtntext]=useState("Order Now");
+    const[counter,setcounter]=useState(0)
     function checkorder(){
-        if(Number(props.price)>400){
-            console.log("Free Delivery")
-        }
-        else{
-            console.log("Delivery Charges added")
-        }
+        // if(Number(props.price)>400){
+        //     console.log("Free Delivery")
+        // }
+        // else{
+        //     console.log("Delivery Charges added")
+        // }
+        setbtntext("Add Item");
+        setcounter(counter+1)
     }
     return(
         <div className="card">
             <img src={props.img}  alt="" className='card-img'/>
             <p>This is {props.name}</p>
             <h3>Price is {props.price}</h3>
-            <button className='btn'onClick={checkorder}>Order Now</button>
+            <button className='btn'onClick={checkorder}>{btntext}{counter}</button>
         </div>
     )
 }
-export default food;
+export default Food;
